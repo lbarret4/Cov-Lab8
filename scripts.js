08aad4dc58549cf13e1429c2a0246a32ca97e5f1
lbarret4 <llewellynbar4@gmail.com>
2018-08-21 00:34:53 -0400

    Added h3-h6 to the DOM

--- scripts.js
+++ scripts.js
@@ -15,4 +15,28 @@ headerDiv.appendChild(h2);
 let h2Text = document.createTextNode('This is an h2');
 h2.appendChild(h2Text);
 document.body.appendChild(h2);
+
+let h3 = document.createElement('h3');
+headerDiv.appendChild(h3);
+let h3Text = document.createTextNode('This is an h3');
+h3.appendChild(h3Text);
+document.body.appendChild(h3);
+
+let h4 = document.createElement('h4');
+headerDiv.appendChild(h4);
+let h4Text = document.createTextNode('This is an h4');
+h4.appendChild(h4Text);
+document.body.appendChild(h4);
+
+let h5 = document.createElement('h5');
+headerDiv.appendChild(h5);
+let h5Text = document.createTextNode('This is an h5');
+h5.appendChild(h5Text);
+document.body.appendChild(h5);
+
+let h6 = document.createElement('h6');
+headerDiv.appendChild(h6);
+let h6Text = document.createTextNode('This is an h6');
+h6.appendChild(h6Text);
+document.body.appendChild(h6);
 });
