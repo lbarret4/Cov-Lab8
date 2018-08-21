aedeb1f9395ac2cbec06d0e2f6e25b51db804886
lbarret4 <llewellynbar4@gmail.com>
2018-08-21 00:10:15 -0400

    Create div with class using DOM manipulation

---
+++ scripts.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded',function(){
+
+let headerDiv = document.createElement('div');
+headerDiv.setAttribute('class','header-container');
+document.body.appendChild(headerDiv);
+});
